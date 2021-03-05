19222f57c2935cc4c136ea78066c541b2fda6706
kiibo <shomok0106k@gmail.com>
2021-03-05 12:33:48 +0900

    feat: socketio-sample

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const http = require("http").createServer(app);
+const io   = require("socket.io")(http);
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -22,20 +23,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
 module.exports = app;
+
+
+io.on("connection", (socket)=>{
+  console.log("ユーザーが接続しました");
+
+  socket.on("post", (msg)=>{
+    io.emit("member-post", msg);
+  });
+});
+
+app.listen(3000)
