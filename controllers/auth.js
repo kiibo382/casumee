1d455b0692ae81bb1b6de4db4393f4ec8e4043a2
shota <shomok0106k@gmail.com>
2021-03-08 14:52:23 +0900

    feat: api specifications

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,13 +5,13 @@ import crypto from 'crypto';
 
 export function login(req, res) {
     try {
-        let refreshId = req.body.userId + secret;
-        let salt = crypto.randomBytes(16).toString('base64');
-        let hash = crypto.createHmac('sha512', salt).update(refreshId).digest("base64");
+        const refreshId = req.body.userId + secret;
+        const salt = crypto.randomBytes(16).toString('base64');
+        const hash = crypto.createHmac('sha512', salt).update(refreshId).digest("base64");
         req.body.refreshKey = salt;
-        let token = jsonwebtoken.sign(req.body, secret);
-        let b = Buffer.from(hash);
-        let refresh_token = b.toString('base64');
+        const token = jsonwebtoken.sign(req.body, secret);
+        const b = Buffer.from(hash);
+        const refresh_token = b.toString('base64');
         res.status(201).send({ accessToken: token, refreshToken: refresh_token });
     } catch (err) {
         res.status(500).send({ errors: err });
@@ -21,7 +21,7 @@ export function login(req, res) {
 export function refresh_token(req, res) {
     try {
         req.body = req.jwt;
-        let token = jsonwebtoken.sign(req.body, secret);
+        const token = jsonwebtoken.sign(req.body, secret);
         res.status(201).send({ id: token });
     } catch (err) {
         res.status(500).send({ errors: err });
