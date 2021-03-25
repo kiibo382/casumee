99fc616a219e2f79f3e9b66804ae12e8224c3487
shota <shomok0106k@gmail.com>
2021-03-25 12:00:51 +0900

    remove: mongoose_command

--- mongoose_command/model/updateone.js
+++
@@ -1,5 +0,0 @@
-Model.updateOne = function updateOne(conditions, doc, options, callback) {
-    _checkContext(this, 'updateOne');
-
-    return _update(this, 'updateOne', conditions, doc, options, callback);
-};
