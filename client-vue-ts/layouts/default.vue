c6c3a6569ad081590c2c72ff94d6892b13152056
kiibo <shomok0106k@gmail.com>
2021-03-26 22:54:06 +0900

    feat: client pages

--- client-vue-ts/layouts/default.vue
+++ client-vue-ts/layouts/default.vue
@@ -24,23 +24,7 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <Header />
     <v-main>
       <v-container>
         <nuxt />
@@ -56,14 +40,18 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    <Footer />
   </v-app>
 </template>
 
 <script>
+import Header from '~/components/Header'
+import Footer from '~/components/Footer'
 export default {
+  components: {
+    Header,
+    Footer,
+  },
   data() {
     return {
       clipped: false,
